fc1bc14c9173d292c043478b47b00c9206757e9d
SaraYohannes <sara.seacucumber@gmail.com>
2024-11-19 10:46:02 +0100

    added PlayerInteract_LMouseOnScreen_Script.cs
    for watching for LMB clicks and sending ref of GameObjects to BoomScript.cs

---
+++ Assets/PlayerInteract_LMouseOnScreen_Script.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// INTENT: Watches for Player using Left Mouse Button clicks on Objects in the Scene
+//         then passes the reference to other scripts
+public class PlayerInteract_LMouseOnScreen_Script : MonoBehaviour
+{
+    /*
+     * NEEDS:
+     * - Raycaster
+     * - Watch for Left Mouse Button clicks
+     * - Get Ref. for Object
+     *   - Pass to BoomScript
+     *   - If Player right Mouse Button clicks, item is 'deselected' and Ref for Object is cleared
+     */ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
