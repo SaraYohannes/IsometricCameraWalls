b22518043aa2de3667c5710f9563bf95838edac2
SaraYohannes <sara.seacucumber@gmail.com>
2024-12-04 16:34:34 +0100

    added picture reference to the blogposts

--- JS_scripts/blogpost_template.js
+++ JS_scripts/blogpost_template.js
@@ -1,13 +1,20 @@
 
 const updates_no = [
-    { postXX_no: 1, date: "01 jan 2024", title: "OPPDATERINGSTITTEL", summary: "SAMMENDRAG!", content: "INNHOLD", gallery: "GALLERI" },
-    { postXX_no: 2, date: "01 feb 2024", title: "OPPDATERINGSTITTEL", summary: "SAMMENDRAG!", content: "INNHOLD", gallery: "GALLERI" }
+    { nr: 1, post: 1, src: 'img_src/dinnerflower.JPG', title: 'en vase med blomster på et middagsbord', alt: 'dinner flower', ptext: 'a vase of wildflowers. Wine in the background. Table is set for dinner', width: "100%", height: "100%" },
+    { nr: 2, post: 1, src: 'img_src/rivercrossing.JPG', title: 'et sted å krysse elven', alt: 'river crossing', ptext: 'a place to cross the river. Not safe', width: "100%", height: "100%" },
+    { nr: 3, post: 2, src: 'img_src/mountainbush.JPG', title: 'busker som skygger for stien', alt: 'mountain bushes', ptext: 'there is a path here, but it is not visible', width: "100%", height: "100%" },
+    { nr: 4, post: 3, src: 'img_src/lakeislet.JPG', title: 'en liten øy i en innsjø', alt: 'lake islet', ptext: 'its not an islet, but it is a lake. The small rock could in this picture look like an islet', width: "100%", height: "100%" },
+    { nr: 5, post: 3, src: 'img_src/redberry.JPG', title: 'et umodent bær', alt: 'red berry', ptext: 'a marsh berry. It is not ripe', width: "100%", height: "100%" },
+    { nr: 6, post: 4, src: 'img_src/plataugrassy.JPG', title: 'et platå', alt: 'a platau', ptext: 'it is not a platau, but in this angle, it looks like it', width: "100%", height: "100%" }
 ];
 
 const updates_en = [
-    { postXX_en: 1, date: "01 jan 2024", title: "TITLE OF UPDATE 1", summary: "SUMMARY!", content: "CONTENT", gallery: "GALLERY" },
-    { postXX_en: 2, date: "01 feb 2024", title: "TITLE OF UPDATE 2", summary: "SUMMARY!", content: "CONTENT", gallery: "GALLERY" },
-    { postXX_en: 3, date: "05 mar 2024", title: "TITLE OF UPDATE 3", summary: "A short summary of what's in the content so you can make an informed decision if you want to read this or not!", content: "A long string of content. You need to know all of these things to be able to follow along on my project development! And there are tonnes of links to external sources and references. I will also add pictures so it will be easier to follow along and you can understand and visualise what I want to achieve", gallery: "GALLERY with picutres and descriptions." }
+    { nr: 1, post: 1, src: 'img_src/dinnerflower.JPG', title: 'a vase of wildflowers on a dining table', alt: 'dinner flower', ptext: 'a vase of wildflowers. Wine in the background. Table is set for dinner', width: "100%", height: "100%" },
+    { nr: 2, post: 1, src: 'img_src/rivercrossing.JPG', title: 'a place to cross the river', alt: 'river crossing', ptext: 'a place to cross the river. Not safe', width: "100%", height: "100%" },
+    { nr: 3, post: 2, src: 'img_src/mountainbush.JPG', title: 'bushes obscouring mountain path', alt: 'mountain bushes', ptext: 'there is a path here, but it is not visible', width: "100%", height: "100%" },
+    { nr: 4, post: 3, src: 'img_src/lakeislet.JPG', title: 'an islet on a lake', alt: 'lake islet', ptext: 'its not an islet, but it is a lake. The small rock could in this picture look like an islet', width: "100%", height: "100%" },
+    { nr: 5, post: 3, src: 'img_src/redberry.JPG', title: 'an unripe berry', alt: 'red berry', ptext: 'a marsh berry. It is not ripe', width: "100%", height: "100%" },
+    { nr: 6, post: 4, src: 'img_src/plataugrassy.JPG', title: 'a platau', alt: 'a platau', ptext: 'it is not a platau, but in this angle, it looks like it', width: "100%", height: "100%" }
 ];
 
 const container_element = document.getElementById("blogpost_wrapper");
