b0cc73816eb6fedd48e563e950c00bb4307a9e39
SaraYohannes <sara.seacucumber@gmail.com>
2024-12-04 10:52:41 +0100

    Revert "added json file for img populating array in small_gallery_template.js"
    This reverts commit 6d269961bd888d0d51bed81b24c984ddf43a0106.

--- JS_scripts/small_gallery_template.js
+++ JS_scripts/small_gallery_template.js
@@ -1,11 +1,9 @@
-
-const complete_gallery = [];
-
-fetch('img_info.json').then(imgpost => {
-    complete_gallery = imgpost;
-}).catch(error => {
-    console.error('getting json file didnt go well', error);
-});
+const complete_gallery = [
+    { nr: 1, post: 1, src: '', title: '', alt: 'this is picture 1', ptext: 'a short description of intent' },
+    { nr: 2, post: 1, src: '', title: '', alt: 'this is picture 2', ptext: 'describing the illustration' },
+    { nr: 3, post: 2, src: '', title: '', alt: 'this is picture 3', ptext: 'some math for interested' },
+    { nr: 4, post: 3, src: '', title: '', alt: 'this is picture 4', ptext: 'current progress, interesting bug' }
+];
 
 const container_whole_gallery = document.getElementById("illmod");
 
