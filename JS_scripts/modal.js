9b5c6d63213be70d9cc18cd3d1c02d12f6ee54c7
SaraYohannes <sara.seacucumber@gmail.com>
2024-12-09 09:18:46 +0100

    update

--- JS_scripts/modal.js
+++ JS_scripts/modal.js
@@ -2,12 +2,14 @@
 function open_large_gallery() {
     const lgallery_holder = document.body;
     // if section w class 'galleryModal' exists, clean it and add a new one
+
+
     if (document.querySelector('galleryModal'))
     {
-        let clean = document.documentElement('galleryModal');
+        let clean = document.querySelector('galleryModal');
         clean.remove();
-
     }
+
     const lgallery_container = document.createElement('section');
     lgallery_container.classList.add('galleryModal');
 
