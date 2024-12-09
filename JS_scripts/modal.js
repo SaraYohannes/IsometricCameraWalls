758dd334e8dca4a0b839ffc9a2beb19c48fa2f4f
SaraYohannes <sara.seacucumber@gmail.com>
2024-12-09 09:13:34 +0100

    update

--- JS_scripts/modal.js
+++ JS_scripts/modal.js
@@ -1,10 +1,17 @@
 
 function open_large_gallery() {
-    const lgallery_holder = document.getElementById('maincontent')
+    const lgallery_holder = document.body;
+    // if section w class 'galleryModal' exists, clean it and add a new one
+    if (document.documentElement('galleryModal'))
+    {
+        let clean = document.documentElement('galleryModal');
+        clean.remove();
 
+    }
     const lgallery_container = document.createElement('section');
     lgallery_container.classList.add('galleryModal');
 
     lgallery_holder.appendChild(lgallery_container);
+
 }
 
