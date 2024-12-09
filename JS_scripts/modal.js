9c54927f793cd9e9247fdea3ec7d8ef2508e9894
SaraYohannes <sara.seacucumber@gmail.com>
2024-12-09 08:26:12 +0100

    added CSS classes and HTML button to test modal script

---
+++ JS_scripts/modal.js
@@ -0,0 +1,9 @@
+
+function open_large_gallery() {
+    const lgallery_holder = document.getElementsByName('body');
+
+    const lgallery_container = document.createElement('section');
+    lgallery_container.classList.add('galleryModal');
+
+}
+
