551e449bc230f54baa1f2f093647839b7357ebb1
SaraYohannes <sara.seacucumber@gmail.com>
2024-12-09 08:27:11 +0100

    appended child

--- JS_scripts/modal.js
+++ JS_scripts/modal.js
@@ -5,5 +5,6 @@ function open_large_gallery() {
     const lgallery_container = document.createElement('section');
     lgallery_container.classList.add('galleryModal');
 
+    lgallery_holder.appendChild(lgallery_container);
 }
 
